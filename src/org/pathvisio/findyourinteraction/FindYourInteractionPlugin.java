9d6441686d7aee3a7a8d435b0b3d542e532f88e1
Martina Summer-Kutmon <mkutmon@gmail.com>
2018-04-25 16:18:12 +0200

    fix #1
    bug fix, code clean, added license header

--- src/org/pathvisio/findyourinteraction/FindYourInteractionPlugin.java
+++ src/org/pathvisio/findyourinteraction/FindYourInteractionPlugin.java
@@ -1,3 +1,19 @@
+//  FindYourInteraction Plugin for PathVisio
+//	Find metabolic reaction from Rhea
+//	Copyright 2016-2018 BiGCaT, Maastricht University
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+   
 package org.pathvisio.findyourinteraction;
 
 import javax.swing.JPopupMenu;
@@ -7,32 +23,36 @@ import org.pathvisio.desktop.PvDesktop;
 import org.pathvisio.desktop.plugin.Plugin;
 import org.pathvisio.gui.PathwayElementMenuListener.PathwayElementMenuHook;
 
+/**
+ * PathVisio plugin class
+ * Registering menu items and actions
+ * @author anwesha, jonathan, mkutmon
+ *
+ */
 public class FindYourInteractionPlugin implements Plugin{
 	private PvDesktop desktop;
+	private PathwayElementMenuHook menuHook;
 	
 	@Override
 	public void init(final PvDesktop desktop) {
-		// TODO Auto-generated method stub
 		this.desktop = desktop;
-		desktop.addPathwayElementMenuHook(new PathwayElementMenuHook(){
-                  /**
+		
+		menuHook = new PathwayElementMenuHook() {	
+			@Override
+			public void pathwayElementMenuHook(VPathwayElement e, JPopupMenu menu) {
+				SearchInteractionAction showInfoAction = new SearchInteractionAction(desktop, e);
+				menu.add (showInfoAction);
+			}
+		};
+		desktop.addPathwayElementMenuHook(new PathwayElementMenuHook() {
+                  /** 
                    * This method is called whenever the user right-clicks
                    * on an element in the Pathway.
                    * VPathwayElement contains the object that was clicked on.
                    */
 				@Override
-				public void pathwayElementMenuHook(VPathwayElement e,
-						JPopupMenu menu) {
-					// TODO Auto-generated method stub
-					// We instantiate an Action
-					SearchInteractionAction showInfoAction = new SearchInteractionAction(desktop);
-                    
-                    // pass the clicked element to the action object
-                    showInfoAction.setElement(e);
-//                    desktop.getSwingEngine().getEngine().getActivePathway().get
-                    // Insert action into the menu.
-                    // NB: this is optional, we can choose e.g.
-                    // to insert only when the clicked element is a certain type.
+				public void pathwayElementMenuHook(VPathwayElement e, JPopupMenu menu) {
+					SearchInteractionAction showInfoAction = new SearchInteractionAction(desktop, e);
                     menu.add (showInfoAction);
 				}
           });
@@ -40,8 +60,6 @@ public class FindYourInteractionPlugin implements Plugin{
 
 	@Override
 	public void done() {
-		// TODO Auto-generated method stub
-//		desktop.dispose();;
+		desktop.getSwingEngine().getApplicationPanel().getPathwayElementMenuListener().removePathwayElementMenuHook(menuHook);
 	}
-
 }
--- src/org/pathvisio/findyourinteraction/SearchInteractionAction.java
+++ src/org/pathvisio/findyourinteraction/SearchInteractionAction.java
@@ -1,3 +1,18 @@
+//  FindYourInteraction Plugin for PathVisio
+//	Find metabolic reaction from Rhea
+//	Copyright 2016-2018 BiGCaT, Maastricht University
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 package org.pathvisio.findyourinteraction;
 
 import java.awt.event.ActionEvent;
@@ -36,14 +51,15 @@ import org.pathvisio.gui.dialogs.DatabaseSearchDialog;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
+/**
+ * Search feature for looking up accession
+ * numbers of reactions/interactions.
+ * @author anwesha,jonathan,mkutmon
+ *
+ */
 public class SearchInteractionAction extends AbstractAction{
 
-	/**
-	 * Search feature for looking up accession
-	 * numbers of reactions/interactions.
-	 */
-	private static final long serialVersionUID = -4231646730955550080L;
-	private String rheaWS = "http://www.rhea-db.org/rest/1.0/ws/reaction/cmlreact?q=";
+	private static String RHEA_BASE_URL = "http://www.rhea-db.org/rest/1.0/ws/reaction/cmlreact?q=";
 	private VPathwayElement elt;
 	private IDMapperStack mapper;
 	private PathwayElement queryElement = null;
@@ -53,121 +69,61 @@ public class SearchInteractionAction extends AbstractAction{
 	private PathwayElement input;
 	
 
-	public SearchInteractionAction(PvDesktop desktop) {
+	public SearchInteractionAction(PvDesktop desktop, VPathwayElement vpe) {
 		// This will be the label of the pop up menu item.
 		putValue (NAME, "Find Your Interaction");
-		putValue (SHORT_DESCRIPTION,"Search the online Rhea database for references,"
-				+ " based on the identifiers of the interactors");
+		putValue (SHORT_DESCRIPTION,"Search the online Rhea database for references, based on the identifiers of the interactors");
+		
 		this.desktop = desktop;
+		this.elt = vpe;
 		mapper = desktop.getSwingEngine().getGdbManager().getCurrentGdb();
-	}
-	/**
-	 * This should be called before the action is triggered 
-	 */
-	public void setElement (VPathwayElement anElt)
-	{
-		elt = anElt;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-//		VPathway vp = elt.getDrawing();
-//		component = (VPathwaySwing)vp.getWrapper();
 		if(elt instanceof Graphics) {
 			PathwayElement p = ((Graphics)elt).getPathwayElement();
 			if (p instanceof MLine){
 				this.input = p;
-				search(p.getPathway(), p.getStartGraphRef(),
-						p.getEndGraphRef());
+				search(p.getPathway(), p.getStartGraphRef(), p.getEndGraphRef());
 			}
 		}
 	}
+	
 	/**
-//	 * Search for identifiers for the selected interaction in Rhea
-//	 * (http://www.rhea-db.org/home) based on identifiers of the nodes that are
-//	 * connected by the interaction
-//	 * 
-//	 * @param pwy
-//	 */
-	private void search(Pathway pwy, final String startNode,
-			final String endNode) {
+	 * Search for identifiers for the selected interaction in Rhea
+	 * (http://www.rhea-db.org/home) based on identifiers of the nodes that are
+	 * connected by the interaction
+	 */
+	private void search(Pathway pwy, final String startNode, final String endNode) {
 		String startNodeId = getElementId(startNode, pwy);
 		String endNodeId = getElementId(endNode, pwy);
 		String query = "";
-//		System.out.println(startNode+"\t"+endNode);
-//		System.out.println(startNodeId+"\t"+endNodeId);
 		/*
 		 * Eg. query: http://www.rhea-db.org/rest/1.0/ws/reaction?q=glucose *
 		 * http://www.rhea-db.org/rest/1.0/ws/reaction?q=CHEBI:17632
 		 * http://www.rhea-db.org/rest/1.0/ws/reaction?q=CHEBI:17632+CHEBI:16301
 		 */
-		if ((startNode != null && !"".equals(startNodeId.trim()))
-				&& (endNode != null && !"".equals(endNodeId.trim())) ){
+		if((startNode != null && !"".equals(startNodeId.trim())) && (endNode != null && !"".equals(endNodeId.trim())) ){
 			if (tries == 0) {
-				query = rheaWS + startNodeId.trim() + "+" + endNodeId.trim();
-				System.out.println(query+"	1");
+				query = RHEA_BASE_URL + startNodeId.trim() + "+" + endNodeId.trim();
 			} else if (tries == 1) {
-				query = rheaWS + startNodeId.trim();
-				System.out.println(query+"	2");
+				query = RHEA_BASE_URL + startNodeId.trim();
 			} else if (tries == 2) {
-				query = rheaWS + endNodeId.trim();
-				System.out.println(query+"	3");
+				query = RHEA_BASE_URL + endNodeId.trim();
 			}
-		}		
-		else if ((startNode != null && endNode == null && !"".equals(startNodeId.trim()))) {
-			int dialogResult = 
-					JOptionPane.showConfirmDialog
-					(null, "Only the start node is connected.\n Do you still want to search ?",
-							"Warning",JOptionPane.YES_NO_OPTION);
+		} else if ((startNode != null && endNode == null && !"".equals(startNodeId.trim()))) {
+			int dialogResult = JOptionPane.showConfirmDialog(null, "Only the start node is connected.\n Do you still want to search ?", "Warning",JOptionPane.YES_NO_OPTION);
 			if(dialogResult == JOptionPane.YES_OPTION){
-				query = rheaWS + startNodeId.trim();
-				System.out.println(query+"	4");
+				query = RHEA_BASE_URL + startNodeId.trim();
 			}
 		} else if ((endNode != null && startNode == null && !"".equals(endNodeId.trim()))) {
-			int dialogResult = 
-					JOptionPane.showConfirmDialog
-					(null, "Only the end node is connected.\n Do you still want to search ?",
-							"Warning",JOptionPane.YES_NO_OPTION);
+			int dialogResult = JOptionPane.showConfirmDialog(null, "Only the end node is connected.\n Do you still want to search ?", "Warning",JOptionPane.YES_NO_OPTION);
 			if(dialogResult == JOptionPane.YES_OPTION){
-				query = rheaWS + endNodeId.trim();
-				System.out.println(query+"	5");
+				query = RHEA_BASE_URL + endNodeId.trim();
 			}
-		}
-//		else if ( "".equals(endNodeId.trim()) && "".equals(startNodeId.trim( )      )   ) {
-//			JOptionPane.showMessageDialog
-//			(null, "Both nodes are not correctly annotated",
-//					"Error",JOptionPane.ERROR_MESSAGE);
-//		}
-//		else if ((startNodeId != null && !"".equals(startNodeId.trim( )    )  )   ) {
-//			int dialogResult = 
-//					JOptionPane.showConfirmDialog
-//					(null, "Only the start node is annotated.\n"
-//							+ "Annotating the datanodes will improve the results.\n"
-//							+ "Do you still want to search ?",
-//							"Warning",JOptionPane.YES_NO_OPTION);
-//			if(dialogResult == JOptionPane.YES_OPTION){
-//				query = rheaWS + startNodeId.trim();
-//				System.out.println(query+"	6");
-//			}
-//		}
-//		else if ((endNodeId != null && !"".equals(endNodeId.trim()))) {
-//			int dialogResult = 
-//					JOptionPane.showConfirmDialog
-//					(null, "Only the end node is annotated.\n"
-//							+ "Annotating the datanodes will improve the results.\n"
-//							+ "Do you still want to search ?",
-//							"Warning",JOptionPane.YES_NO_OPTION);
-//			if(dialogResult == JOptionPane.YES_OPTION){
-//				query = rheaWS + endNodeId.trim();
-//				System.out.println(query+"	7");
-//			}
-//		}		
-		else{
-			JOptionPane.showMessageDialog
-					(null, "Search function for this pathway element has not been implemented yet",
-							"Error",JOptionPane.ERROR_MESSAGE);
+		} else {
+			JOptionPane.showMessageDialog(null, "Search function for this pathway element has not been implemented yet", "Error",JOptionPane.ERROR_MESSAGE);
 		}
 
 		tries++;
@@ -178,146 +134,125 @@ public class SearchInteractionAction extends AbstractAction{
 		ProgressDialog dialog = new ProgressDialog(desktop.getSwingEngine().getFrame(),
 				"Searching Rhea for interactions. Query =" + query, progress,
 				true, true);
-		/// TODO
-//		dialog.setLocation( new InteractionDialog(desktop.getSwingEngine(),(Graphics) elt.getPathwayElement(),component) );
 		
-		if ( ! query.equals("")){
-		SwingWorker<List<XrefWithSymbol>, Void> sw = new SwingWorker<List<XrefWithSymbol>, Void>() {
-			protected List<XrefWithSymbol> doInBackground() throws IDMapperException {
-				// The result set
-				List<XrefWithSymbol> result = new ArrayList<XrefWithSymbol>();
-				// querying Rhea using webservice				
-				try {
-					DocumentBuilderFactory dbf = DocumentBuilderFactory
-							.newInstance();
-					DocumentBuilder db = dbf.newDocumentBuilder();
-
-					URL queryText = new URL(text);
-					Document doc = db.parse(queryText.openStream());
-
-					String text2parse = doc.getDocumentElement()
-							.getTextContent();
-					text2parse = text2parse.replaceAll(
-							"^\\s+|\\s+$|\\s*(\n)\\s*|(\\s)\\s*", "$1$2")
-							.replace("\t", " ");
-
-					if (text2parse.contains("rhea")) {
-						String[] parsedText = text2parse.split("\n");
-
-						for (int i = 0; i < parsedText.length; i = i + 4) {
-							/*
-							 * Get id
-							 */
-
-							Xref intxref = new Xref(parsedText[i],
-									DataSource.getExistingBySystemCode("Rh"));
-							/*
-							 * Get uri
-							 */
-							String interactionUri = parsedText[i + 2];
-
-							Document doc2 = null;
-							String reactionText = "";
-							try {								
-								DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-								doc2 =    factory.newDocumentBuilder().
-										parse(new URL(interactionUri).openStream());
-								reactionText = doc2.getElementsByTagName("name").item(0).getTextContent();
-							} catch (Exception e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							result.add(new XrefWithSymbol(intxref, reactionText));
-
-						}
-					} else {
-						if (tries < 3) { // Threshold up to 3 to run all the case
-							search(input.getPathway(), input.getStartGraphRef(),
-									input.getEndGraphRef());
-						} else {
-							System.out.println("No reactions");
-						}
-				}
-
-				} catch (ParserConfigurationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SAXException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				return result;
-			}
-
-			private void applyAutoFill(XrefWithSymbol ref) {
-				input.setElementID(ref.getId());
-				input.setDataSource(ref.getDataSource());
-//				idText.setText(ref.getId());
-//				dsm.setSelectedItem(ref.getDataSource());
-			}
-
-			@Override
-			public void done() {
-				progress.finished();
-				if (!progress.isCancelled()) {
-					List<XrefWithSymbol> results = null;
+		if (!query.equals("")) {
+			SwingWorker<List<XrefWithSymbol>, Void> sw = new SwingWorker<List<XrefWithSymbol>, Void>() {
+				protected List<XrefWithSymbol> doInBackground() throws IDMapperException {
+					// The result set
+					List<XrefWithSymbol> result = new ArrayList<XrefWithSymbol>();
+					// querying Rhea using webservice				
 					try {
-						results = get();
-						// Show results to user
-						if (results != null && results.size() > 0) {
-							String pretext = "Reactions shown for ";
-							DatabaseSearchDialog resultDialog = new DatabaseSearchDialog(
-									pretext + text.replace(rheaWS, ""), results);
-							resultDialog.setVisible(true);
-							XrefWithSymbol selected = resultDialog
-									.getSelected();
-							if (selected != null) {
-								applyAutoFill(selected);
+						DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+						DocumentBuilder db = dbf.newDocumentBuilder();
+	
+						URL queryText = new URL(text);
+						Document doc = db.parse(queryText.openStream());
+	
+						String text2parse = doc.getDocumentElement().getTextContent();
+						text2parse = text2parse.replaceAll(
+								"^\\s+|\\s+$|\\s*(\n)\\s*|(\\s)\\s*", "$1$2")
+								.replace("\t", " ");
+	
+						if (text2parse.contains("rhea")) {
+							String[] parsedText = text2parse.split("\n");
+	
+							for (int i = 0; i < parsedText.length; i = i + 4) {
+								Xref intxref = new Xref(parsedText[i], DataSource.getExistingBySystemCode("Rh"));
+		
+								String interactionUri = parsedText[i + 2];
+	
+								Document doc2 = null;
+								String reactionText = "";
+								try {								
+									DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+									doc2 = factory.newDocumentBuilder().parse(new URL(interactionUri).openStream());
+									reactionText = doc2.getElementsByTagName("name").item(0).getTextContent();
+								} catch (Exception e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+								result.add(new XrefWithSymbol(intxref, reactionText));
+	
 							}
 						} else {
-							String posttext = "";
-							String pretext = "No reactions found for ";
-							if (!((text.contains("CHEBI")|| (text.contains("\\d+"))))) {
-								posttext = ".\nAnnotating the datanodes will improve the results.";
+							if (tries < 3) { // Threshold up to 3 to run all the case
+								search(input.getPathway(), input.getStartGraphRef(),
+										input.getEndGraphRef());
+							} else {
+								System.out.println("No reactions");
 							}
-							if (text.isEmpty()) {
-								pretext = "Search function for this pathway element has not been implemented yet";
+					}
+	
+					} catch (ParserConfigurationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (SAXException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	
+					return result;
+				}
+	
+				private void applyAutoFill(XrefWithSymbol ref) {
+					input.setElementID(ref.getId());
+					input.setDataSource(ref.getDataSource());
+				}
+	
+				@Override
+				public void done() {
+					progress.finished();
+					if (!progress.isCancelled()) {
+						List<XrefWithSymbol> results = null;
+						try {
+							results = get();
+							// Show results to user
+							if (results != null && results.size() > 0) {
+								String pretext = "Reactions shown for ";
+								DatabaseSearchDialog resultDialog = new DatabaseSearchDialog(pretext + text.replace(RHEA_BASE_URL, ""), results, null);
+								resultDialog.setVisible(true);
+								XrefWithSymbol selected = resultDialog.getSelected();
+								if (selected != null) {
+									applyAutoFill(selected);
+								}
+							} else {
+								String posttext = "";
+								String pretext = "No reactions found for ";
+								if (!((text.contains("CHEBI")|| (text.contains("\\d+"))))) {
+									posttext = ".\nAnnotating the datanodes will improve the results.";
+								}
+								if (text.isEmpty()) {
+									pretext = "Search function for this pathway element has not been implemented yet";
+								}
+								JOptionPane.showMessageDialog(null,
+										pretext + text.replace(RHEA_BASE_URL, "")
+										+ posttext);
 							}
+						} catch (InterruptedException e) {
+							// Ignore, thread interrupted. Same as cancel.
+						} catch (ExecutionException e) {
 							JOptionPane.showMessageDialog(null,
-									pretext + text.replace(rheaWS, "")
-									+ posttext);
+									"Exception occurred while searching,\n"
+											+ "see error log for details.",
+											"Error", JOptionPane.ERROR_MESSAGE);
+							Logger.log.error("Error while searching", e);
 						}
-					} catch (InterruptedException e) {
-						// Ignore, thread interrupted. Same as cancel.
-					} catch (ExecutionException e) {
-						JOptionPane.showMessageDialog(null,
-								"Exception occurred while searching,\n"
-										+ "see error log for details.",
-										"Error", JOptionPane.ERROR_MESSAGE);
-						Logger.log.error("Error while searching", e);
 					}
 				}
-			}
-		};
-		sw.execute();
-		
-		dialog.setVisible(true);
+			};
+			sw.execute();
+			dialog.setVisible(true);
 		}
-		
 	}
-	/*
-	 * Select query id (identifier or label)
-	 */
+
 	private String getElementId(String nodeRef, Pathway pwy) {
 		boolean matchFound = false;
-//		System.out.println("1 inside "+nodeRef);
 		String id = "";
 		if (nodeRef != null) {
 			for (PathwayElement pe : pwy.getDataObjects()) {
@@ -354,15 +289,11 @@ public class SearchInteractionAction extends AbstractAction{
 						ids = mapper.mapID(queryElement.getXref(), DataSource.getExistingBySystemCode("S"));
 					}
 					if(ids.isEmpty()){
-//						id = "";
 						id = queryElement.getTextLabel();
 						id = id.replaceAll(" ", "+");
 					}else{
 						String tmpid = ids.iterator().next().getId();
 						if(ids.iterator().next().getDataSource() == DataSource.getExistingBySystemCode("S")){
-								/*
-								 * Choose ids starting with P/Q if found
-								 */
 								for(Xref ref:ids){
 									if(ref.getId().startsWith("P")||ref.getId().startsWith("Q")){
 										tmpid = ref.getId();
@@ -371,7 +302,6 @@ public class SearchInteractionAction extends AbstractAction{
 							}
 						id = tmpid;
 					}
-					
 				} catch (IDMapperException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -381,10 +311,8 @@ public class SearchInteractionAction extends AbstractAction{
 						id = "CHEBI:" + id;
 					}
 				}
-
 			}
 		}
-//		System.out.println("1 id:"+id);
 		return id;
 	}
 	private PathwayElement findConnectedNode(MAnchor anchor, Pathway pwy) {
@@ -394,8 +322,7 @@ public class SearchInteractionAction extends AbstractAction{
 			if (pwe.getObjectType() == ObjectType.LINE) {
 				if (anchor.getGraphId().equalsIgnoreCase(pwe.getEndGraphRef())) {
 					targetNodeRef = pwe.getStartGraphRef();
-				} else if (anchor.getGraphId().equalsIgnoreCase(
-						pwe.getStartGraphRef())) {
+				} else if (anchor.getGraphId().equalsIgnoreCase(pwe.getStartGraphRef())) {
 					targetNodeRef = pwe.getEndGraphRef();
 				}
 			}
@@ -409,6 +336,5 @@ public class SearchInteractionAction extends AbstractAction{
 		}
 		return targetNode;
 	}
-	
 }
 
